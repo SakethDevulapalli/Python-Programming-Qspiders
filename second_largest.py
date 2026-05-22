b6eda4dadfd2ab420586febc7b29d826d56d2d9b
Saketh <sakethdevulapalli@gmail.com>
2026-05-22 12:20:27 +0530

    second_largest.py file update

--- second_largest.py
+++ second_largest.py
@@ -4,12 +4,16 @@ num3 = int(input("Enter the third number : "))
 
 if num1 > num2 and num1 > num3 :
     if num2 > num3 :
-        print(num2, "is greatest number..")
+        print(num2, "is second greatest number..")
     else :
-        print(num3, "is greatest number..")
+        print(num3, "is second greatest number..")
 elif num2 > num3 and num2 > num1 :
-    if num1 > num3 :
-        print(num3, "is greatest number..")
+    if num3 > num1 :
+        print(num3, "is second greatest number..")
+    else :
+        print(num1, "is second greatest number..")
 elif num3 > num1 and num3 > num2 :
-    if num2 > num1 :
-        print(num3, "is greatest number..")
+    if num1 > num2 :
+        print(num1, "is second greatest number..")
+    else :
+        print(num2, "is second greatest number..")
